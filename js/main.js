09a0449f09637059b2e42087ccf37b68e88c2e63
Александр <sh889@ya.ru>
2022-02-19 16:45:27 +0500

    Получение данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+// import { posts } from './data.js';
 import { renderPosts } from './preview.js';
 import './upload-form.js';
+import { request } from './network.js';
 
-renderPosts();
+const onFail = (error) => {
+  console.log(error);
+};
+
+request(renderPosts, onFail, 'GET');
